444d625ebc3bc2a2c988eede278f88db1449d2ac
Joseph Ligier <joseph.ligier@accenture.com>
2024-09-23 15:27:25 +0200

    Fix tf plan

--- infra/toto/ite/main.tf
+++ infra/toto/ite/main.tf
@@ -1,5 +1,22 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
 
-aaaa
+  required_version = ">= 1.0.0"
+}
 
+provider "random" {}
 
-aaaaaaa
+resource "random_string" "example" {
+  length  = 16
+  special = false
+}
+
+output "random_string_output" {
+  value = random_string.example.result
+}
+
