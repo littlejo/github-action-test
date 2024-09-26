304e605594450cae1945a2e7d38362f74a907e16
Joseph Ligier <joseph.ligier@accenture.com>
2024-09-26 11:55:24 +0200

    test

--- infra/_sharedservices/main.tf
+++ infra/_sharedservices/main.tf
@@ -2,3 +2,14 @@ resource "local_file" "foo" {
   content  = "foo!"
   filename = "${path.module}/foo.bar"
 }
+
+terraform { 
+  cloud { 
+    
+    organization = "test-jli2" 
+
+    workspaces { 
+      name = "test-github-action" 
+    } 
+  } 
+}
